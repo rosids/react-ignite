ceced552b55cfc57672f230134e82e866f88a5a4
Rosiele David <rosieleserpa@hotmail.com>
2022-09-23 17:32:55 -0300

    adicionando propriedades ao componente Post

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,14 +1,16 @@
-import { Post } from "./Post";
+import { Post } from "./Post"
 
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Rosiele David"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia ex, recusandae obcaecati magnam molestiae reiciendis ut neque dolorum, voluptates maiores laboriosam maxime explicabo veritatis architecto natus. Quis veniam animi hic?"
+      />
+      <Post
+        author="Diego Fernandes"
+        content="Um novo post muito legal"
+      />
     </div>
-  );
+  )
 }
