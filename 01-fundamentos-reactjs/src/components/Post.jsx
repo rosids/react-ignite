1bf08b4e8f7fa9a8bfb7f31bc453389ca396fce3
Rosiele David <rosieleserpa@hotmail.com>
2022-09-24 12:21:02 -0300

    add estilização no componente Post

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -20,9 +20,13 @@ export function Post() {
 
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da </p>Rocketseat. O nome do projeto é DoctorCare 🚀
 
-        <p>👉 <a href="">jane.design/doctorcare</a></p>
+        <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
 
-        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
       </div>
     </article>
   )
