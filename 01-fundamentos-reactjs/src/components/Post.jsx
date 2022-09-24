bd31f2923d0e83044a6f3f2e77cd9ac4f6512eec
Rosiele David <rosieleserpa@hotmail.com>
2022-09-24 14:15:10 -0300

    add componente Avatar e sua estilização

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
@@ -7,7 +8,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/rosids.png" />
+          <Avatar src="https://github.com/rosids.png" />
           <div className={styles.authorInfo}>
             <strong>Rosiele David</strong>
             <span>Web Developer</span>
