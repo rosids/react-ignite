0c4cc0c6bc29b48cb4a96673b051213b82039c4b
Rosiele David <rosieleserpa@hotmail.com>
2022-09-27 17:11:09 -0300

    inserindo comentários no Post

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -8,7 +8,9 @@ import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post({author, publishedAt, content}) {
-  const [comments, setComments] = useState([1, 2]);
+  const [comments, setComments] = useState(['Post muito bacana, hein?!']);
+
+  const [newCommentText, setNewCommentText] = useState('');
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' H:mm'h'", { locale: ptBR });
 
@@ -17,7 +19,12 @@ export function Post({author, publishedAt, content}) {
   function handleCreateNewComment(event) {
     event.preventDefault();
 
-    setComments([...comments, comments.length + 1])
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange(event) {
+    setNewCommentText(event.target.value);
   }
 
   return (
@@ -48,7 +55,10 @@ export function Post({author, publishedAt, content}) {
         <strong>Deixe seu feedback</strong>
 
         <textarea
+          name="comment"
           placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -58,7 +68,7 @@ export function Post({author, publishedAt, content}) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment />
+          return <Comment content={comment} />
         })}
       </div>
     </article>
