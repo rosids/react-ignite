8fd2067ded08b6d9f67608fb7c833b70c20c64ef
Rosiele David <rosieleserpa@hotmail.com>
2022-09-28 17:11:58 -0300

    envia função de deletar comentário do componente Post para o Comment

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/rosids.png" alt="" />
@@ -16,7 +20,7 @@ export function Comment({ content }) {
               <time title="11 de Maio às 10:53" dateTime="2022-09-24 10:53:10">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
